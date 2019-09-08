a6038ea89c5f09e2d4b35cc2b51a37f6da9a9194
carolexc <37673283+carolexc@users.noreply.github.com>
2019-09-08 15:58:29 -0300

    Criação das entidade de Tipo de Evento.

---
+++ src/main/java/br/com/ft/gdp/controller/EventTypeController.java
@@ -0,0 +1,88 @@
+package br.com.ft.gdp.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ft.gdp.event.CreatedResourceEvent;
+import br.com.ft.gdp.models.domain.EventType;
+import br.com.ft.gdp.models.dto.EventTypeDTO;
+import br.com.ft.gdp.service.EventTypeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * 
+* Classe EventTypeController.java
+*
+* @author <a href="carolexc@gmail.com">Caroline Aguiar</a>
+*
+* @since 8 de set de 2019
+ */
+@Api("Endpoint - Tipo Evento")
+@RestController
+@RequestMapping(value = "/eventType")
+public class EventTypeController {
+
+    @Autowired
+    private EventTypeService service;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @ApiOperation(nickname = "eventtype-get", value = "Busca uma página de responsáveis")
+    @GetMapping
+    public ResponseEntity<Page<EventType>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                      @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+                                                      @RequestParam(value = "orderBy", defaultValue = "id") String orderBy,
+                                                      @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+        Pageable pageSettings = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        return ResponseEntity.ok(service.searchEntityPage(pageSettings));
+    }
+
+    @ApiOperation(nickname = "responsible-post", value = "Insere um novo responsável na aplicação")
+    @PostMapping
+    public ResponseEntity<EventTypeDTO> persist(@Validated @RequestBody(required = true) EventTypeDTO eventType,
+                                                  HttpServletResponse response) {
+        EventType createdEventType = service.persist(eventType.getEventTypeDomainFromDTO());
+
+        publisher.publishEvent(new CreatedResourceEvent(this, response, createdEventType.getId()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdEventType.getEventTypeDTOFromDomain());
+
+    }
+
+    @ApiOperation(nickname = "responsible-put", value = "Atualiza um responsável na aplicação")
+    @PutMapping("/{id}")
+    public ResponseEntity<EventTypeDTO> update(@PathVariable("id") Long id,
+                                                 @Validated @RequestBody(required = true) EventTypeDTO eventType,
+                                                 HttpServletResponse response) {
+        EventType createdResponsible = service.update(id, eventType.getEventTypeDomainFromDTO());
+
+        return ResponseEntity.ok().body(createdResponsible.getEventTypeDTOFromDomain());
+
+    }
+
+    @ApiOperation(nickname = "eventtype-get-id", value = "Busca um responsável baseano no identificador")
+    @GetMapping("/{id}")
+    public ResponseEntity<EventType> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+}
---
+++ src/main/java/br/com/ft/gdp/models/dto/EventTypeDTO.java
@@ -0,0 +1,52 @@
+package br.com.ft.gdp.models.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import br.com.ft.gdp.models.domain.EventType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+* Classe EventTypeDTO.java
+*
+* @author <a href="carolexc@gmail.com">Caroline Aguiar</a>
+*
+* @since 8 de set de 2019
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class EventTypeDTO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2370290606342755763L;
+
+	private Long id;
+
+    @NotBlank(message = "Informar o NOME  do evento é obrigatório")
+    @Size(min = 3, max = 45, message = "O nome do evento deve conter ao menos três letras")
+    private String name;
+
+    @Size(min = 3, max = 200, message = "A descricao do evento deve conter ao menos três letras")
+    private String description;
+
+    @JsonIgnore
+    public EventType getEventTypeDomainFromDTO() {
+    	EventType domain = new EventType();
+    	
+        domain.setId(getId());
+        domain.setName(getName());
+        domain.setDescription(getDescription());
+        return domain;
+    }
+}
