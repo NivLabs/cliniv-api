7198a8894a6f1c976eebdf0f5bccf843cbcca028
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2019-10-02 17:10:05 -0300

    Corrigindo mapeamento

--- src/main/java/br/com/ft/gdp/models/domain/Anamnese.java
+++ src/main/java/br/com/ft/gdp/models/domain/Anamnese.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -22,35 +24,36 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-public class Anamnese extends BaseObject{	
-	private static final long serialVersionUID = -4203582800741543902L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name = "ID_VISITA")
-	private Visit idVisit;
-	
-	@Column(name = "ID_PACIENTE")
-	private Patient idPatient;
-	
-	@Column(name = "ANAMNESIS_ITEM")
-	private AnamnesisItem idAnamnesisItem;
-	
-	@Column(name = "RESPOSTA")
-	private String response;
-		
-	@JsonIgnore
-	public AnamneseDTO getAnamneseDTOFromDomain() {
-		AnamneseDTO dto = new AnamneseDTO();
-		
-		dto.setId(getId());
-		dto.setIdAnamnesisItem(getIdAnamnesisItem());
-		dto.setIdPatient(getIdPatient());
-		dto.setIdVisit(getIdVisit());
-		dto.setResponse(getResponse());
-		
-		return dto;
-	}
+public class Anamnese extends BaseObject {
+    private static final long serialVersionUID = -4203582800741543902L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ID_VISITA")
+    private Visit idVisit;
+
+    @Column(name = "ID_PACIENTE")
+    private Patient idPatient;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_ANAMNESE_ITEM")
+    private AnamnesisItem idAnamnesisItem;
+
+    @Column(name = "RESPOSTA")
+    private String response;
+
+    @JsonIgnore
+    public AnamneseDTO getAnamneseDTOFromDomain() {
+        AnamneseDTO dto = new AnamneseDTO();
+
+        dto.setId(getId());
+        dto.setIdAnamnesisItem(getIdAnamnesisItem());
+        dto.setIdPatient(getIdPatient());
+        dto.setIdVisit(getIdVisit());
+        dto.setResponse(getResponse());
+
+        return dto;
+    }
 }
