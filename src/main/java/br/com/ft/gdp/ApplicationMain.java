f1d33ae588a1956f39c90e0be3374805b491e1f6
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2019-10-11 15:38:03 -0300

    Adicionando TimeZone padrão para a aplicação

--- src/main/java/br/com/ft/gdp/ApplicationMain.java
+++ src/main/java/br/com/ft/gdp/ApplicationMain.java
@@ -1,10 +1,24 @@
 package br.com.ft.gdp;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApplicationMain {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ApplicationMain.class, args);
     }
