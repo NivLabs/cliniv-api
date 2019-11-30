1f2b12f183051c16e75cfc551f4f5ab58c8f0be5
Vinícios Rodrigues <vinicios.araujo@linx.com.br>
2019-11-30 18:11:31 -0300

    Criação da tabela e objeto [Instituto]

---
+++ src/main/java/br/com/ft/gdp/models/domain/Institute.java
@@ -0,0 +1,82 @@
+package br.com.ft.gdp.models.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * Classe Maintainer.java
+ * 
+ * @author <a href="mailto:viniciosarodrigues@gmail.com">Vinícios Rodrigues</a>
+ * 
+ * @since 30 de nov de 2019
+ */
+
+@Entity
+@Table(name = "INSTITUTO")
+@Data
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Institute implements Serializable {
+
+    private static final long serialVersionUID = -8389926388826078313L;
+
+    @Id
+    @Column(name = "CNPJ")
+    private String cnpj;
+
+    @Column(name = "CNES")
+    private String cnes;
+
+    @Column(name = "NOME")
+    private String name;
+
+    @Column(name = "NOME_CORPORATIVO")
+    private String corporativeName;
+
+    @Column(name = "NATUREZA LEGAL")
+    private String legalNature;
+
+    @Column(name = "LOGRADOURO")
+    private String street;
+
+    @Column(name = "NUMERO")
+    private String addressNumber;
+
+    @Column(name = "COMPLEMENTO")
+    private String complement;
+
+    @Column(name = "CEP")
+    private String postalCode;
+
+    @Column(name = "ESTADO")
+    private String state;
+
+    @Column(name = "BAIRRO")
+    private String neighborhood;
+
+    @Column(name = "CIDADE")
+    private String city;
+
+    @Column(name = "TELEFONE")
+    private String phone;
+
+    @Column(name = "DEPENDENCIA")
+    private String dependency;
+
+    @Column(name = "TIPO_DE_INSTITUICAO")
+    private String instituteType;
+
+    @Column(name = "GESTAO")
+    private String management;
+
+}
