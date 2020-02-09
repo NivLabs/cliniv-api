523b57e16ecd03b67668952d50298540e9b0429d
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-02-09 12:46:17 -0300

    Criação dos DTOs para responsáveis

--- src/main/java/br/com/ft/gdp/models/dto/ResponsibleDTO.java
+++ src/main/java/br/com/ft/gdp/models/dto/ResponsibleDTO.java
@@ -1,12 +1,5 @@
 package br.com.ft.gdp.models.dto;
 
-import java.io.Serializable;
-import java.util.Date;
-
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
-
-import br.com.ft.gdp.models.enums.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,24 +15,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
-public class ResponsibleDTO implements Serializable {
+@EqualsAndHashCode(callSuper = true)
+public class ResponsibleDTO extends PersonDTO {
     private static final long serialVersionUID = -5141572031863436326L;
 
-    private Long id;
-
-    private String firstName;
-
-    private String lastName;
-
-    private String rg;
-
-    private String cpf;
-
-    @DateTimeFormat(iso = ISO.DATE)
-    private Date bornDate;
-
-    private String principalNumber;
-
-    private Gender gender;
+    private String professionalIdentity;
 }
