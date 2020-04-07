bfa6da742b8a971a56a5bf0d6e66ecc7fecb7a75
viniciosarodrigues <viniciosarodrigues@gmail.com>
2020-04-06 23:35:33 -0300

    Criando objeto base para transferências via rest

---
+++ src/main/java/br/com/ft/gdp/models/dto/DataTransferObjectBase.java
@@ -0,0 +1,15 @@
+package br.com.ft.gdp.models.dto;
+
+import java.io.Serializable;
+
+/**
+ * Objeto base para todas as requisições e respostas
+ * 
+ * @author viniciosarodrigues
+ *
+ */
+public class DataTransferObjectBase implements Serializable {
+
+    private static final long serialVersionUID = 8601475769419769218L;
+
+}
