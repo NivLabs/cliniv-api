6103bba776677d6fb95abd957704868885b5be29
viniciosarodrigues <viniciosarodrigues@gmail.com>
2020-04-06 23:39:52 -0300

    Criando fluxo de Usuário

---
+++ src/main/java/br/com/ft/gdp/controller/UserController.java
@@ -0,0 +1,44 @@
+package br.com.ft.gdp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ft.gdp.controller.utils.FilteredPageable;
+import br.com.ft.gdp.models.dto.DataTransferObjectBase;
+import br.com.ft.gdp.models.dto.UserDTO;
+import br.com.ft.gdp.service.UserService;
+import io.swagger.annotations.Api;
+
+/**
+ * Classe SpecialityController.java
+ * 
+ * @author <a href="mailto:viniciosarodrigues@gmail.com">Vinícios Rodrigues</a>
+ * 
+ * @since 6 de jan de 2020
+ */
+@Api("Endpoint - Usuário")
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('COMUM', 'ADMIN')")
+    public ResponseEntity<Page<UserDTO>> findPage(FilteredPageable pageSettings) {
+        return ResponseEntity.ok(userService.searchEntityPage(pageSettings));
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('COMUM', 'ADMIN')")
+    public ResponseEntity<DataTransferObjectBase> findById(@PathVariable(name = "id") Long id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+}
