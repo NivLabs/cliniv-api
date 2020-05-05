9741775373c95a4d05e7b2f875532251a871d3c7
Vinícios A. Rodrigues <viniciosarodrigues@gmail.com>
2020-05-04 23:33:25 -0300

    feat: Centralizando tempo de expiração do token (#79)
    * feat: Reestruturando papéis de acesso
    
    * doc: atualizando documentação
    
    * feat: Criando múltiplos perfis de execução
    
    * doc: Atualizando README
    
    * doc: Atualização do Readme
    
    * feat: Centralizando tempo de expiração do token

--- src/main/java/br/com/ft/gdp/config/JwtProfileConfiguration.java
+++ src/main/java/br/com/ft/gdp/config/JwtProfileConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@ConfigurationProperties("jwt")
+@ConfigurationProperties("jwt.secret")
 public class JwtProfileConfiguration {
 
     @Profile("heroku")
