7de04d3a9f4ec789c2eeee30a7df7b819596fec8
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-08-03 21:37:25 -0300

    [feat] Mapeamento de entidade relacional com OPERADORA e PLANO #54

---
+++ src/main/java/br/com/nivlabs/gp/enums/Modality.java
@@ -0,0 +1,11 @@
+package br.com.nivlabs.gp.enums;
+
+/**
+ * Modalidades de operadoras de saúde
+ * 
+ * @author viniciosarodrigues
+ *
+ */
+public enum Modality {
+    HEALTH_PLAN_OPERATORS, MEDICAL_OR_DENTAL_COOPERATIVE, SELF_MANAGEMENT, PHILANTHROPY, ADMNISTRATOR, HEALTH_INSURERS
+}
