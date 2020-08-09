01cdf066fb3bc61165fc02c2d10a96a0c3a441c4
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-08-09 14:54:08 -0300

    [bug] Ajuste em script para MariaDB

--- src/main/resources/db/migration/V000083__CRIACAO_DE_ESTRUTURA_BASE_OPERADORA.sql
+++ src/main/resources/db/migration/V000083__CRIACAO_DE_ESTRUTURA_BASE_OPERADORA.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `OPERADORA` (
+CREATE TABLE OPERADORA (
   `ID` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `RAZAO_SOCIAL` VARCHAR(300) NOT NULL,
   `COD_ANS` BIGINT(20) NOT NULL,
@@ -6,5 +6,5 @@ CREATE TABLE `OPERADORA` (
   `NOME_FANTASIA` VARCHAR(300) NOT NULL,
   `MODALIDADE` VARCHAR(45) NOT NULL COMMENT 'Operadoras possíveis:\n- Operadoras de planos de saúde\n- Cooperativa médica/odontológica\n- Autogestão\n- Filantropia\n- Administradora\n- Seguradoras de saúde',
   PRIMARY KEY (`ID`),
-  UNIQUE INDEX `COD_ANS_UNIQUE` (`COD_ANS` ASC) VISIBLE,
-  UNIQUE INDEX `CNPJ_UNIQUE` (`CNPJ` ASC) VISIBLE);
+  UNIQUE KEY `COD_ANS_UK` (`COD_ANS` ASC),
+  UNIQUE KEY `CNPJ_UK` (`CNPJ` ASC));
