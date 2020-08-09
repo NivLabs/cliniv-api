34ca2af7f3876a6e6b57006cfa44c906299383d3
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-08-09 11:44:15 -0300

    [bug] Correção em script de produção

--- src/main/resources/db/migration/V000083__CRIACAO_DE_ESTRUTURA_BASE_OPERADORA.sql
+++ src/main/resources/db/migration/V000083__CRIACAO_DE_ESTRUTURA_BASE_OPERADORA.sql
@@ -4,7 +4,7 @@ CREATE TABLE `OPERADORA` (
   `COD_ANS` BIGINT(20) NOT NULL,
   `CNPJ` VARCHAR(14) NOT NULL,
   `NOME_FANTASIA` VARCHAR(300) NOT NULL,
-  `MODALIDADE` VARCHAR(45) NOT NULL COMMENT 'Operadoras possíveis:\n- Operadoras de planos de saúde\n- Cooperativa médica/odontológica\n- Autogestão\n- Filantropia\n- Administradora\n- Seguradoras de saúde',
+  `MODALIDADE` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE INDEX `COD_ANS_UNIQUE` (`COD_ANS` ASC) VISIBLE,
   UNIQUE INDEX `CNPJ_UNIQUE` (`CNPJ` ASC) VISIBLE);
