53e129834edfa6c9b9a46df30f452156f0eab4d6
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-09-12 20:31:53 -0300

    [feat] Adicionando histórico de atendimentos nas infos do paciente

--- src/main/java/br/com/nivlabs/gp/service/PatientService.java
+++ src/main/java/br/com/nivlabs/gp/service/PatientService.java
@@ -52,6 +52,8 @@ public class PatientService implements GenericService {
     private PatientAllergyRepository patientAllergyDao;
     @Autowired
     private HealthPlanRepository healthPlanDao;
+    @Autowired
+    private AttendanceService attendanceService;
 
     @Autowired
     private PersonService personService;
@@ -88,13 +90,26 @@ public class PatientService implements GenericService {
             healthPlan.setOperatorName(patient.getHealthPlan().getHealthOperator().getFantasyName());
             patientInfo.setHealthPlan(healthPlan);
         }
-
         BeanUtils.copyProperties(patient, patientInfo, Patient_.ALLERGIES);
-
         patient.getAllergies().forEach(allergy -> patientInfo.getAllergies().add(allergy.getDescription()));
+        setPatientHistory(patientInfo);
 
         return patientInfo;
 
+    }
+
+    /**
+     * Adiciona o histórico do paciente
+     * 
+     * @param patientInfo
+     */
+    private void setPatientHistory(PatientInfoDTO patientInfo) {
+        try {
+            logger.info("Buscando histórico de atendimentos do paciente...");
+            patientInfo.getAttendanceHistory().addAll(attendanceService.getAttandenceByPatientId(patientInfo.getId()));
+        } catch (HttpException e) {
+            logger.info("Nenhum atendimento para o paciente...");
+        }
     }
 
     /**
@@ -115,6 +130,7 @@ public class PatientService implements GenericService {
             BeanUtils.copyProperties(personFromDb.getAddress(), address);
             patientInfo.setAddress(address);
         }
+        setPatientHistory(patientInfo);
 
         return patientInfo;
     }
@@ -150,6 +166,8 @@ public class PatientService implements GenericService {
             }
 
             BeanUtils.copyProperties(patient, patientInfo);
+            setPatientHistory(patientInfo);
+
             return patientInfo;
         } catch (HttpException e) {
             return findPersonByCpf(cpf);
@@ -186,6 +204,8 @@ public class PatientService implements GenericService {
             patientInfo.setHealthPlan(healthPlan);
         }
         BeanUtils.copyProperties(patient, patientInfo);
+        setPatientHistory(patientInfo);
+
         return patientInfo;
     }
 
