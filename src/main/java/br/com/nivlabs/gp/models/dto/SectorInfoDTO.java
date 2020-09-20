7fcb0b6e37bf618e2dccbfe51d6425c5f1edc94a
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-09-20 20:26:33 -0300

    [feat] Ajuste no cadastro de setores

--- src/main/java/br/com/nivlabs/gp/models/dto/SectorInfoDTO.java
+++ src/main/java/br/com/nivlabs/gp/models/dto/SectorInfoDTO.java
@@ -35,85 +35,85 @@ public class SectorInfoDTO extends DataTransferObjectBase {
     @DateTimeFormat(iso = ISO.DATE)
     private LocalDateTime createdAt;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public List<AccommodationDTO> getListOfRoomsOrBeds() {
-		return listOfRoomsOrBeds;
-	}
+    public List<AccommodationDTO> getListOfRoomsOrBeds() {
+        return listOfRoomsOrBeds;
+    }
 
-	public void setListOfRoomsOrBeds(List<AccommodationDTO> listOfRoomsOrBeds) {
-		this.listOfRoomsOrBeds = listOfRoomsOrBeds;
-	}
+    public void setListOfRoomsOrBeds(List<AccommodationDTO> listOfRoomsOrBeds) {
+        this.listOfRoomsOrBeds = listOfRoomsOrBeds;
+    }
 
-	public LocalDateTime getCreatedAt() {
-		return createdAt;
-	}
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
-	public void setCreatedAt(LocalDateTime createdAt) {
-		this.createdAt = createdAt;
-	}
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((listOfRoomsOrBeds == null) ? 0 : listOfRoomsOrBeds.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((listOfRoomsOrBeds == null) ? 0 : listOfRoomsOrBeds.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		SectorInfoDTO other = (SectorInfoDTO) obj;
-		if (createdAt == null) {
-			if (other.createdAt != null)
-				return false;
-		} else if (!createdAt.equals(other.createdAt))
-			return false;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (listOfRoomsOrBeds == null) {
-			if (other.listOfRoomsOrBeds != null)
-				return false;
-		} else if (!listOfRoomsOrBeds.equals(other.listOfRoomsOrBeds))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SectorInfoDTO other = (SectorInfoDTO) obj;
+        if (createdAt == null) {
+            if (other.createdAt != null)
+                return false;
+        } else if (!createdAt.equals(other.createdAt))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (listOfRoomsOrBeds == null) {
+            if (other.listOfRoomsOrBeds != null)
+                return false;
+        } else if (!listOfRoomsOrBeds.equals(other.listOfRoomsOrBeds))
+            return false;
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return "SectorInfoDTO [id=" + id + ", description=" + description + ", listOfRoomsOrBeds=" + listOfRoomsOrBeds
-				+ ", createdAt=" + createdAt + "]";
-	}
-    
+    @Override
+    public String toString() {
+        return "SectorInfoDTO [id=" + id + ", description=" + description + ", listOfRoomsOrBeds=" + listOfRoomsOrBeds
+                + ", createdAt=" + createdAt + "]";
+    }
+
 }
