ebafbf0da6754609f2c27220b714c8c687f30aa8
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-09-23 22:33:27 -0300

    [refactory] Refatoração de documento

--- src/main/java/br/com/nivlabs/gp/enums/ContractType.java
+++ src/main/java/br/com/nivlabs/gp/enums/ContractType.java
@@ -7,7 +7,10 @@ package br.com.nivlabs.gp.enums;
  *
  */
 public enum ContractType {
+    // Individual ou familiar
     INDIVIDUAL_OR_FAMILY,
+    // Coletivo 
     MEMBERSHIP_COLLECTIVE,
+    // Coletivo Empresarial
     BUSINESS_COLLECTIVE
 }
