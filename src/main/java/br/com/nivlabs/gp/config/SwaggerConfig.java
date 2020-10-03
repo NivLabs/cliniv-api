b6a7e5acec889c67f3041c0497ff2a8b7e86cfd5
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-10-03 16:56:00 -0300

    [feat] Adicionando códigos padrões para 400 e 401

--- src/main/java/br/com/nivlabs/gp/config/SwaggerConfig.java
+++ src/main/java/br/com/nivlabs/gp/config/SwaggerConfig.java
@@ -36,10 +36,13 @@ public class SwaggerConfig {
     private final ResponseMessage m201 = simpleMessage(201, "Recurso criado");
     private final ResponseMessage m204put = simpleMessage(204, "Atualização ok");
     private final ResponseMessage m204del = simpleMessage(204, "Deleção ok");
+    private final ResponseMessage m400 = simpleMessage(400, "Requisição mal formada");
+    private final ResponseMessage m401 = simpleMessage(401, "Não autenticado");
     private final ResponseMessage m403 = simpleMessage(403, "Não autorizado");
     private final ResponseMessage m404 = simpleMessage(404, "Não encontrado");
+    private final ResponseMessage m409 = simpleMessage(409, "Requisição conflitante");
     private final ResponseMessage m422 = simpleMessage(422, "Erro de validação");
-    private final ResponseMessage m500 = simpleMessage(500, "Erro inesperado");
+    private final ResponseMessage m500 = simpleMessage(500, "Erro não esperado");
 
     private ResponseMessage simpleMessage(int code, String msg) {
         return new ResponseMessageBuilder().code(code).message(msg).build();
@@ -48,10 +51,10 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
-                .globalResponseMessage(RequestMethod.GET, Arrays.asList(m403, m404, m500))
-                .globalResponseMessage(RequestMethod.POST, Arrays.asList(m201, m403, m422, m500))
-                .globalResponseMessage(RequestMethod.PUT, Arrays.asList(m204put, m403, m404, m422, m500))
-                .globalResponseMessage(RequestMethod.DELETE, Arrays.asList(m204del, m403, m404, m500))
+                .globalResponseMessage(RequestMethod.GET, Arrays.asList(m400, m401, m403, m404, m500))
+                .globalResponseMessage(RequestMethod.POST, Arrays.asList(m201, m400, m401, m403, m409, m422, m500))
+                .globalResponseMessage(RequestMethod.PUT, Arrays.asList(m204put, m400, m401, m403, m404, m409, m422, m500))
+                .globalResponseMessage(RequestMethod.DELETE, Arrays.asList(m204del, m400, m401, m403, m404, m409, m500))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.nivlabs.gp.controller"))
                 .paths(PathSelectors.any())
