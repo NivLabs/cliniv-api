73c07244a4ebc9ad1788ab019dddf7eb372bc0b3
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-10-25 10:35:13 -0300

    [feat] Criando setor/leito padrão para evento de atendimento

---
+++ src/main/resources/db/migration/V000100__ATT_DESRCRICAO_TIPOS_DE_EVENTOS.sql
@@ -0,0 +1,3 @@
+UPDATE `TIPO_EVENTO` SET `DESCRICAO` = 'Anulação de prescrição' WHERE (`ID` = '9');
+UPDATE `TIPO_EVENTO` SET `DESCRICAO` = 'Criação de prescrição' WHERE (`ID` = '8');
+UPDATE `TIPO_EVENTO` SET `ID_TIPO_EVENTO` = '', `NOME` = 'REALIZA_PROCEDIMENTO', `DESCRICAO` = 'Realiza procedimento' WHERE (`ID` = '10');
