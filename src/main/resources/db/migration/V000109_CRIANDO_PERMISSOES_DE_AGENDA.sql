ea9b2c131d5593db4da70adec1f4a1bd8fe2a68e
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-12-27 23:14:06 -0300

    [FEAT] Criando permissões para agenda

---
+++ src/main/resources/db/migration/V000109_CRIANDO_PERMISSOES_DE_AGENDA.sql
@@ -0,0 +1,2 @@
+INSERT INTO `PERMISSAO` (`ID`, `NOME`, `DESCRICAO`) VALUES (20, 'ROLE_AGENDA_LEITURA', 'Permissão de leitura em agenda');
+INSERT INTO `PERMISSAO` (`ID`, `NOME`, `DESCRICAO`) VALUES (20, 'ROLE_AGENDA_ESCRITA', 'Permissão de escrita em agenda');
