c00dc62c993fd25ab30dc4dca0eab1329ed305a2
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2021-01-07 21:51:01 -0300

    [feat] Add consulta de agendamento por ID

--- src/main/java/br/com/nivlabs/gp/controller/patient/ScheduleController.java
+++ src/main/java/br/com/nivlabs/gp/controller/patient/ScheduleController.java
@@ -2,17 +2,26 @@ package br.com.nivlabs.gp.controller.patient;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.nivlabs.gp.controller.filters.ScheduleFilters;
+import br.com.nivlabs.gp.event.CreatedResourceEvent;
 import br.com.nivlabs.gp.models.dto.ScheduleInfoDTO;
 import br.com.nivlabs.gp.service.ScheduleService;
 import io.swagger.annotations.Api;
@@ -25,14 +34,36 @@ public class ScheduleController {
 
     @Autowired
     private ScheduleService principalService;
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
     @ApiOperation(nickname = "schedule-get", value = "Busca informações de agendamentos baseados num filtro")
     @GetMapping
-    @PreAuthorize("hasAnyRole('AGENDA_LEITURA', 'PACIENTE_LEITURA', 'ATENDIMENTO_ESCRITA', 'ATENDIMENTO_LEITURA', 'ADMIN')")
+    @PreAuthorize("hasAnyRole('AGENDA_LEITURA', 'AGENDA_ESCRITA', 'ATENDIMENTO_ESCRITA', 'ATENDIMENTO_LEITURA', 'ADMIN')")
     public ResponseEntity<List<ScheduleInfoDTO>> getSchedulesByFilters(ScheduleFilters filters) {
         Pageable pageSettings = PageRequest.of(filters.getPage(), filters.getSize(),
                                                Direction.valueOf(filters.getDirection()), filters.getOrderBy());
         return ResponseEntity.ok(principalService.findByFilters(filters, pageSettings));
+    }
+
+    @ApiOperation(nickname = "schedule-get", value = "Busca um agendamento")
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('AGENDA_ESCRITA', 'AGENDA_LEITURA', 'ATENDIMENTO_ESCRITA', 'ATENDIMENTO_LEITURA', 'ADMIN')")
+    public ResponseEntity<ScheduleInfoDTO> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(principalService.findById(id));
+
+    }
+
+    @ApiOperation(nickname = "schedule-post", value = "Insere um novo agendamento na aplicação")
+    @PostMapping
+    @PreAuthorize("hasAnyRole('AGENDA_ESCRITA', 'ADMIN')")
+    public ResponseEntity<ScheduleInfoDTO> create(@Validated @RequestBody(required = true) ScheduleInfoDTO schedule,
+                                                  HttpServletResponse response) {
+        ScheduleInfoDTO created = principalService.create(schedule);
+
+        publisher.publishEvent(new CreatedResourceEvent(this, response, created.getId()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
 }
--- src/main/java/br/com/nivlabs/gp/service/ScheduleService.java
+++ src/main/java/br/com/nivlabs/gp/service/ScheduleService.java
@@ -5,13 +5,24 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import br.com.nivlabs.gp.controller.filters.ScheduleFilters;
+import br.com.nivlabs.gp.enums.DocumentType;
+import br.com.nivlabs.gp.exception.HttpException;
+import br.com.nivlabs.gp.models.domain.Person;
+import br.com.nivlabs.gp.models.domain.Person_;
+import br.com.nivlabs.gp.models.domain.Schedule;
+import br.com.nivlabs.gp.models.dto.DocumentDTO;
+import br.com.nivlabs.gp.models.dto.PatientInfoDTO;
+import br.com.nivlabs.gp.models.dto.ResponsibleInfoDTO;
 import br.com.nivlabs.gp.models.dto.ScheduleInfoDTO;
 import br.com.nivlabs.gp.repository.ScheduleRepository;
+import br.com.nivlabs.gp.util.StringUtils;
 
 /**
  * Classe ScheduleService.java
@@ -41,4 +52,69 @@ public class ScheduleService implements GenericService {
         return principalRepo.resumedList(filters, pageRequest).getContent();
     }
 
+    public ScheduleInfoDTO create(ScheduleInfoDTO request) {
+        return null;
+    }
+
+    /**
+     * Busca agendamento por identificador único
+     * 
+     * @param id Identificador único do agendamento
+     * @return Informações detalhadas do agendamento
+     */
+    public ScheduleInfoDTO findById(Long id) {
+        Schedule objFromDb = principalRepo.findById(id)
+                .orElseThrow(() -> new HttpException(HttpStatus.NOT_FOUND, "Agendamento não encontrado!"));
+
+        logger.info("Paciente {} encontrado com sucesso!", objFromDb.getPatient().getPerson().getFullName());
+        ScheduleInfoDTO response = new ScheduleInfoDTO();
+
+        logger.info("Iniciando conversão de entidade Agendamento para resposta...");
+        BeanUtils.copyProperties(objFromDb, response);
+
+        logger.info("Iniciando conversão de entidade Paciente para resposta...");
+        response.setPatient(new PatientInfoDTO());
+        BeanUtils.copyProperties(objFromDb.getPatient().getPerson(), response.getPatient(), Person_.ID);
+        BeanUtils.copyProperties(objFromDb.getPatient(), response.getPatient());
+        handleDocument(objFromDb.getPatient().getPerson(), response.getPatient());
+
+        logger.info("Iniciando conversão de entidade Responsável para resposta...");
+        response.setProfessional(new ResponsibleInfoDTO());
+        BeanUtils.copyProperties(objFromDb.getProfessional().getPerson(), response.getProfessional(), Person_.ID);
+        BeanUtils.copyProperties(objFromDb.getProfessional(), response.getProfessional());
+        handleDocument(objFromDb.getProfessional().getPerson(), response.getProfessional());
+
+        logger.info("Conversões realizadas com sucesso, enviando resposta de volta para o client");
+        return response;
+    }
+
+    /**
+     * Trata informações de documento no paciente
+     * 
+     * @param source
+     * @param target
+     */
+    private void handleDocument(Person source, PatientInfoDTO target) {
+        if (StringUtils.isNullOrEmpty(source.getCpf())) {
+            throw new HttpException(HttpStatus.UNPROCESSABLE_ENTITY,
+                    "O cadastro do paciente está sem CPF informado, favor completar o cadastro antes de realizar um agendamento");
+        }
+        DocumentDTO document = new DocumentDTO(DocumentType.CPF, source.getCpf());
+        target.setDocument(document);
+    }
+
+    /**
+     * Trata informações de documento no responsável (Profissional)
+     * 
+     * @param source
+     * @param target
+     */
+    private void handleDocument(Person source, ResponsibleInfoDTO target) {
+        if (StringUtils.isNullOrEmpty(source.getCpf())) {
+            throw new HttpException(HttpStatus.UNPROCESSABLE_ENTITY,
+                    "O cadastro do paciente está sem CPF informado, favor completar o cadastro antes de realizar um agendamento");
+        }
+        DocumentDTO document = new DocumentDTO(DocumentType.CPF, source.getCpf());
+        target.setDocument(document);
+    }
 }
