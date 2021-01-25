2a4584870901c457a267d1fd9774e9684a165be0
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2021-01-25 10:06:20 -0300

    [feat] Unificação da migração

--- src/main/resources/db/migration/V000062__CRIANDO_NOVOS_PAPEIS_PARA_PARAMETROS.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `PERMISSAO` (`ID`, `NOME`, `DESCRICAO`) VALUES (16, 'ROLE_PARAMETRO_LEITURA', 'Permissão de leitura em parâmetros da aplicação');
-INSERT INTO `PERMISSAO` (`ID`, `NOME`, `DESCRICAO`) VALUES (17, 'ROLE_PARAMETRO_ESCRITA', 'Permissão de escrita em parâmetros da aplicação');
--- src/main/resources/db/migration/V000109__CRIANDO_PERMISSOES_DE_AGENDA.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `PERMISSAO` (`ID`, `NOME`, `DESCRICAO`) VALUES (21, 'ROLE_AGENDA_LEITURA', 'Permissão de leitura em agenda');
-INSERT INTO `PERMISSAO` (`ID`, `NOME`, `DESCRICAO`) VALUES (22, 'ROLE_AGENDA_ESCRITA', 'Permissão de escrita em agenda');
