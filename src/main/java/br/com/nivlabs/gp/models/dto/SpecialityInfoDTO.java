101e872939115e7d70465d6fcdf6759c5b0f4506
Vinícios A. Rodrigues <viniciosarodrigues@gmail.com>
2021-01-26 21:29:28 -0300

    Develop into master (#224)
    * [feat] implementação inicial do novo fluxo de CRUD para Especialidades
    
    * [feat] CRUD para especialidade
    
    * [update] Spring Boot 2.4.2 update
    
    * [update] Spring Boot 2.4.2 update
    
    * [update] Spring Boot 2.4.2 update (#214)
    
    * [feat] Unificação da migração
    
    * Vini branch into develop (#215)
    
    * [update] Spring Boot 2.4.2 update
    
    * [feat] Unificação da migração
    
    * [feat] Adicionando logs de listagem de questões de anamnese
    
    * Vini branch (#217)
    
    * [update] Spring Boot 2.4.2 update
    
    * [feat] Unificação da migração
    
    * [feat] Adicionando logs de listagem de questões de anamnese
    
    * [feat] Ajuste no objeto detalhado de especialidade
    
    * [bug] Correção do update de especialidades
    
    * [bug] Correção do update de especialidades (#221)
    
    * Vini branch into develop (#223)
    
    * [bug] Correção do update de especialidades
    
    * [feat] Melhoria no tratamento de exceptions

--- src/main/java/br/com/nivlabs/gp/models/dto/SpecialityInfoDTO.java
+++ src/main/java/br/com/nivlabs/gp/models/dto/SpecialityInfoDTO.java
@@ -25,8 +25,8 @@ public class SpecialityInfoDTO extends DataTransferObjectBase {
     @ApiModelProperty("Identificador único da especialidade")
     private Long id;
 
+  @ApiModelProperty("Nome da especialidade")
     @NotNull(message = "Informe o nome da especialidade")
-    @Min(value = 3, message = "Informe pelo menos 3 caracteres nonome da especialidade")
     private String name;
 
     @ApiModelProperty("Descrição da especialidade")
@@ -34,12 +34,33 @@ public class SpecialityInfoDTO extends DataTransferObjectBase {
 
     private List<ResponsibleDTO> responsibles = new ArrayList<>();
 
+
+    public SpecialityInfoDTO() {
+        super();
+    }
+
+    public SpecialityInfoDTO(Long id, String name, String description, List<ResponsibleDTO> responsibles) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.responsibles = responsibles;
+    }
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
@@ -59,11 +80,17 @@ public class SpecialityInfoDTO extends DataTransferObjectBase {
     }
 
     @Override
+    public String toString() {
+        return "SpecialityInfoDTO [id=" + id + ", name=" + name + ", description=" + description + ", responsibles=" + responsibles + "]";
+    }
+
+    @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((description == null) ? 0 : description.hashCode());
         result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
         result = prime * result + ((responsibles == null) ? 0 : responsibles.hashCode());
         return result;
     }
@@ -87,6 +114,11 @@ public class SpecialityInfoDTO extends DataTransferObjectBase {
                 return false;
         } else if (!id.equals(other.id))
             return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
         if (responsibles == null) {
             if (other.responsibles != null)
                 return false;
@@ -99,5 +131,4 @@ public class SpecialityInfoDTO extends DataTransferObjectBase {
     public String toString() {
         return "SpecialityInfoDTO [id=" + id + ", description=" + description + ", responsibles=" + responsibles + "]";
     }
-
 }
