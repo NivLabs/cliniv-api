ce0cfeed0fa8c1c82fc9db308eb13877ec58804b
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2021-02-07 18:20:28 -0300

    [feat] Recurso de paginaçãod e anamnese com filtro

---
+++ src/main/java/br/com/nivlabs/gp/repository/custom/anamnesisform/AnamnesisFormRepositoryCustom.java
@@ -0,0 +1,8 @@
+package br.com.nivlabs.gp.repository.custom.anamnesisform;
+
+import br.com.nivlabs.gp.models.dto.AnamnesisFormDTO;
+import br.com.nivlabs.gp.repository.custom.CustomRepository;
+
+public interface AnamnesisFormRepositoryCustom extends CustomRepository<AnamnesisFormDTO> {
+
+}
