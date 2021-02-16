eada88a5d45e8d1c80b2cf58c66ca5891030b305
caroline.aguiar <caroline.aguiar@linx.com.br>
2021-02-15 21:23:42 -0300

    Criação de put e delete de layout de relatório

--- src/main/java/br/com/nivlabs/gp/service/ReportService.java
+++ src/main/java/br/com/nivlabs/gp/service/ReportService.java
@@ -1,11 +1,11 @@
 package br.com.nivlabs.gp.service;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
 import java.sql.Date;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -13,7 +13,6 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -89,16 +88,20 @@ public class ReportService implements GenericService {
 
     }
 
-    public ReportLayoutDTO newReporLayout(String reportName, String description, FileDTO file) {
+    public ReportLayoutDTO newReporLayout(Long id, String reportName, String description, FileDTO file) {
 
         ReportLayoutDTO reportLayoutDTO = new ReportLayoutDTO();
         ReportLayout reportLayout = new ReportLayout();
-        reportLayout.setId(null);
+        reportLayout.setId(id);
         reportLayout.setName(reportName);
         reportLayout.setDescription(description);
         reportLayout.setXml(file.getBase64());
         reportLayout.setCreatedAt(LocalDateTime.now());
-        reportLayout.setParams(readParamsXml(file));
+        try {
+            reportLayout.setParams(readParamsXml(file.getBase64()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         repository.save(reportLayout);
 
         BeanUtils.copyProperties(reportLayout, reportLayoutDTO);
@@ -107,19 +110,18 @@ public class ReportService implements GenericService {
 
     }
 
-    private List<ReportLayoutParameter> readParamsXml(FileDTO file) {
+    private List<ReportLayoutParameter> readParamsXml(String file) throws IOException {
 
         List<ReportLayoutParameter> parameters = new ArrayList<ReportLayoutParameter>();
 
-        Stream<String> lines = null;
 
-        try {
-            lines = Files.lines(Paths.get(file.getUrl()));
-        } catch (IOException e) {
-            logger.error("Erro ao ler xml = " + file.getUrl());
-        }
+        byte[] bytes = Base64.getDecoder().decode(file);
 
-        lines.parallel().forEach(line -> {
+        InputStream reportInputStream = new ByteArrayInputStream(bytes);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(reportInputStream));
+
+        while(reader.ready()) {
+            String line = reader.readLine();
             if (line.startsWith("<parameter")) {
                 ReportLayoutParameter param = new ReportLayoutParameter();
                 if (line.contains("name=")) {
@@ -135,7 +137,7 @@ public class ReportService implements GenericService {
                 parameters.add(param);
 
             }
-        });
+        }
 
         return parameters;
     }
@@ -239,6 +241,17 @@ public class ReportService implements GenericService {
         reportParam.setParams(map);
 
         return reportParam;
+    }
+
+    public void deleteLayoutById(Long id) {
+        this.findById(id);
+        repository.deleteById(id);
+    }
+
+    public ReportLayoutDTO update(Long id, FileDTO file, String reportName, String description) {
+        
+        return this.newReporLayout(id, reportName, description, file);
+        
     }
 
 }
