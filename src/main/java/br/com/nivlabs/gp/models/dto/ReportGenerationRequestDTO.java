df99ea8a6790864b507b7aad40d829e3394a1fdd
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2021-03-25 19:16:59 -0300

    [feat] Melhorando estrutura do DTO de geração de relatórios

--- src/main/java/br/com/nivlabs/gp/models/dto/ReportGenerationRequestDTO.java
+++ src/main/java/br/com/nivlabs/gp/models/dto/ReportGenerationRequestDTO.java
@@ -17,8 +17,52 @@ public class ReportGenerationRequestDTO extends DataTransferObjectBase {
 
     private static final long serialVersionUID = 4069593034981336944L;
 
+    @ApiModelProperty("Identificador único do layout")
+    private Long id;
+
+    @ApiModelProperty("Nome ou título do layout")
+    private String name;
+
+    @ApiModelProperty("Descrição do layout")
+    private String description;
+
+    @ApiModelProperty("Base64 do layout")
+    private String base64;
+
     @ApiModelProperty("Lista de parâmetros do layout")
     private Set<ReportLayoutParameterDTO> params;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBase64() {
+        return base64;
+    }
+
+    public void setBase64(String base64) {
+        this.base64 = base64;
+    }
 
     public Set<ReportLayoutParameterDTO> getParams() {
         return params;
