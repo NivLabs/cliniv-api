cd474ce996c7c5ba1392e54d79549f1f4611c988
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2021-09-30 22:52:27 -0300

    Merge branch 'vini_branch' of https://github.com/NiV-Labs/gestao-de-prontuario.git into vini_branch

--- src/main/java/br/com/nivlabs/gp/controller/DashboardController.java
+++ src/main/java/br/com/nivlabs/gp/controller/DashboardController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.nivlabs.gp.models.dto.DashboardCardsInformationDTO;
-import br.com.nivlabs.gp.service.DashboardService;
+import br.com.nivlabs.gp.service.dashboard.DashboardService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
