7249ea2e3b53f7dfa170095e2fe9e445a493f6ba
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2021-10-12 21:58:58 -0300

    [bug] Cadastro de operadora de saúde retornando erro no CNPJ

--- src/main/java/br/com/nivlabs/gp/util/DocumentValidator.java
+++ src/main/java/br/com/nivlabs/gp/util/DocumentValidator.java
@@ -4,6 +4,9 @@ public class DocumentValidator {
     private static final int[] pesoCPF = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
     private static final int[] pesoCNPJ = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
 
+    private DocumentValidator() {
+    }
+
     private static int calcularDigito(String str, int[] peso) {
         int soma = 0;
         for (int indice = str.length() - 1, digito; indice >= 0; indice--) {
