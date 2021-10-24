9b7ca65bf6dac6a2bd86e5efd38eab8caddca2bc
viniciosarodrigues <viniciosarodrigues@gmail.com>
2021-10-24 00:33:24 -0300

    [feat] Readicionando o ApplicationMain com controle de parâmetros

--- src/main/java/br/com/nivlabs/gp/ApplicationMain.java
+++ src/main/java/br/com/nivlabs/gp/ApplicationMain.java
@@ -4,11 +4,19 @@ import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.nivlabs.gp.repository.ParameterRepository;
+
 @SpringBootApplication
 public class ApplicationMain {
+
+    public static AppSettings SETTINGS;
+
+    @Autowired
+    private ParameterRepository parameterRepository;
 
     @PostConstruct
     public void setTimeZone() {
@@ -18,4 +26,14 @@ public class ApplicationMain {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationMain.class, args);
     }
+
+    @PostConstruct
+    private void loadParameter() {
+        SETTINGS = new AppSettings(parameterRepository.findAll());
+    }
+
+    public static void reloadParameter(ParameterRepository parameterRepository) {
+        SETTINGS = new AppSettings(parameterRepository.findAll());
+    }
+
 }
