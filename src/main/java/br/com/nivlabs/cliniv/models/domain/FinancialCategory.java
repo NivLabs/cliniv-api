7d862766021f613eb22465e45b129b76529b2d74
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2024-03-22 23:10:48 -0300

    #343 Inclusão de processo novo de consulta de relatórios

---
+++ src/main/java/br/com/nivlabs/cliniv/models/domain/FinancialCategory.java
@@ -0,0 +1,50 @@
+package br.com.nivlabs.cliniv.models.domain;
+
+import br.com.nivlabs.cliniv.models.BaseObjectWithId;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "CATEG_LANC_FIN")
+public class FinancialCategory extends BaseObjectWithId<String> {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+    @Column(name = "DESCRICAO")
+    private String description;
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FinancialCategory that = (FinancialCategory) o;
+        return Objects.equals(id, that.id) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
+    }
+}
